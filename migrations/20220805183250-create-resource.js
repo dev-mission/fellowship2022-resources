581f29f27af42d449d3aabbf261187ac01ce7376
Kevin Rodas <kevin@KR-MBP-W.local>
2022-08-05 12:19:28 -0700

    Resource Model Created and created model testing with Fixtures

---
+++ migrations/20220805183250-create-resource.js
@@ -0,0 +1,67 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Resources', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER,
+      },
+      CategoryTitle: {
+        type: Sequelize.STRING,
+      },
+      Title: {
+        type: Sequelize.TEXT,
+      },
+      Organization: {
+        type: Sequelize.TEXT,
+      },
+      NavPosition: {
+        type: Sequelize.INTEGER,
+      },
+      ShortDetails: {
+        type: Sequelize.TEXT,
+      },
+      LongDetails: {
+        type: Sequelize.TEXT,
+      },
+      Eligibility: {
+        type: Sequelize.TEXT,
+      },
+      Hours: {
+        type: Sequelize.TEXT,
+      },
+      Address: {
+        type: Sequelize.TEXT,
+      },
+      Contact: {
+        type: Sequelize.TEXT,
+      },
+      MainImg: {
+        type: Sequelize.STRING,
+      },
+      SuppImg: {
+        type: Sequelize.STRING,
+      },
+      Link: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.sequelize.query('ALTER SEQUENCE "Resources_id_seq" RESTART WITH 100;');
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Resources');
+  },
+};
---
+++ test/models/resource.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const helper = require('../helper');
+const models = require('../../models');
+
+describe('models.Resource', () => {
+  beforeEach(async () => {
+    await helper.loadFixtures(['resources']);
+  });
+
+  it('creates a new Resource record', async () => {
+    let res = models.Resource.build({
+      CategoryId: 1,
+      CategoryTitle: 'This is fixture item 1.',
+      Title: 'This is fixture item 1.',
+      Organization: 'This is fixture item 1.',
+      NavPosition: 1,
+      ShortDetails: 'This is fixture item 1.',
+      LongDetails: 'This is fixture item 1.',
+      Eligibility: 'This is fixture item 1.',
+      Hours: 'This is fixture item 1.',
+      Address: 'This is fixture item 1.',
+      Contact: 'This is fixture item 1.',
+      MainImg: 'This is fixture item image 1.',
+      SuppImg: 'This is fixture item image 1.',
+      Link: 'This is fixture item url 1.',
+    });
+    assert.deepStrictEqual(res.id, null);
+    await res.save();
+    assert(res.id);
+    res = await models.Resource.findByPk(res.id);
+    assert.deepStrictEqual(res.CategoryId, 1);
+    assert.deepStrictEqual(res.CategoryTitle, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.Title, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.Organization, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.NavPosition, 1);
+    assert.deepStrictEqual(res.ShortDetails, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.LongDetails, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.Eligibility, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.Hours, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.Address, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.Contact, 'This is fixture item 1.');
+    assert.deepStrictEqual(res.MainImg, 'This is fixture item image 1.');
+    assert.deepStrictEqual(res.SuppImg, 'This is fixture item image 1.');
+    assert.deepStrictEqual(res.Link, 'This is fixture item url 1.');
+  });
+
+  it('returns all the Resources', async () => {
+    const results = await models.Resource.findAll();
+    assert.deepStrictEqual(results.length, 2);
+  });
+});
