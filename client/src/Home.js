ba6cb791ca9dc1bdf2b50e1f5ae79cd3d147e1cf
Josue Hernandez <joshuamo574@gmail.com>
2022-08-23 16:44:05 -0700

    Displayed Categories

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,21 @@
 import { Link } from 'react-router-dom';
 import { useAuthContext } from './AuthContext';
+import { useEffect, useState } from 'react';
+
 function Home() {
+  const [items, setItems] = useState([]);
+
+  useEffect(function () {
+    fetch('/api/categories')
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   const { user } = useAuthContext();
   return (
     <main className="container">
       <h1>Home</h1>
+  
       {user?.isAdmin && (
          <p>
            <Link to="/detail/new" className="btn btn-primary">
@@ -12,6 +23,13 @@ function Home() {
            </Link>
          </p>
        )}
+   {items.map((item) => (
+           <p>
+             {
+               item.Name
+             }
+           </p>
+         ))}
     </main>
   );
 }
